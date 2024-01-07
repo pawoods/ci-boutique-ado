ddfe11256406a1d9d8c08d319f46779ed1d9379f
pawoods <>
2024-01-07 15:42:52 +0000

    added checkout success logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -9,14 +9,12 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
-
     instance.order.update_total()
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_delete(sender, instance, created, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
-
     instance.order.update_total()
